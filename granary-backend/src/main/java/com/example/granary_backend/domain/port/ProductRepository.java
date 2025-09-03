2f94346322b455f2b7525eabe740bacbe2e06502
levimarkmuigai <levimarkmuigai@gmail.com>
2025-09-03 22:17:17 +0300

    feat( initial commit: findById, findAll, sc )

---
+++ granary-backend/src/main/java/com/example/granary_backend/domain/port/ProductRepository.java
@@ -0,0 +1,14 @@
+ package com.example.granary_backend.domain.port;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.granary_backend.domain.model.Product;
+import com.example.granary_backend.domain.model.ProductId;
+
+ public interface ProductRepository {
+   Optional<Product> findById(ProductId id);
+   List<Product> findAll();
+   void save(Product product);
+   void delete(ProductId id);
+ }
